98332e400e7614a7cdeccf523a17643628395fb7
jerikolagazo <20616804+jerikolagazo@users.noreply.github.com>
2025-12-09 13:43:41 -0800

    rewrote the code that builds the model by moving other components into different files, readjusted the augmentatio paramters, and adjusted the layers.

--- model.py
+++ model.py
@@ -1,73 +1,18 @@
-# Import basic data manipulation, analysis, and visualization libraries.
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-
 # Import various tensorflow libraries for machine learning.
 import tensorflow as tf
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.keras import layers, models, optimizers, mixed_precision
+from tensorflow.keras import layers, models, optimizers,regularizers, mixed_precision
+from sklearn.utils.class_weight import compute_class_weight
 
-# Import various operating system/file interaction libraries.
-from urllib.request import urlretrieve
-from PIL import Image
-import zipfile
-import os
-
-# Define the variables that will be used for downloading, unzipping, and managing the dataset.
-URL = "https://www.kaggle.com/api/v1/datasets/download/ma7555/cat-breeds-dataset"
-ZIP_PATH = "content/cat-breeds-dataset.zip"
-EXTRACTION_PATH = 'content/cat-breeds-dataset'
-
-# Verifies if the dataset exist, if not, it downloads it. 
-if (os.path.exists(f"content/cat-breeds-dataset/images") == False) or (os.path.exists(f"content/cat-breeds-dataset/images") == False):
-    # Download the dataset using the URL and PATH,
-    urlretrieve(URL, ZIP_PATH)
-    # Unzip the downloaded dataset so that we are able to work the dataset.
-    with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref:
-        zip_ref.extractall(EXTRACTION_PATH)
-    # Delete cat-breeds-dataset.zip now that we have the dataset.
-    os.remove(ZIP_PATH)
-
-# Defines variables that will be used to verifying the data.
-invalid_files_removed = 0
-corrupted_files_removed = 0
-verify_files = False
-
-# Goes through every folder and every image in each folder to remove corrupted images or
-# invald files.
-if verify_files:
-    for root, _, files in os.walk('content/cat-breeds-dataset/images'):
-        for file in files:
-            # Verifies if the file is an image.
-            if file.lower().endswith(('jpg', 'jpeg', 'png')):
-                filepath = os.path.join(root, file)
-                # Attempts to load the image to verify validity.
-                try:
-                    with Image.open(filepath) as img:
-                        img.load()
-                # Removes the file if it is corrupted.
-                except Exception as e:
-                    os.remove(filepath)
-                    corrupted_files_removed += 1
-            # Removes the file if it is not a .jpg, .jpeg, or .png.    
-            else:
-                os.remove(filepath)
-                invalid_files_removed += 1
-
-    print(f"Invalid files removed: {invalid_files_removed}")
-    print(f"Corrupted files removed: {corrupted_files_removed}")
-
-
-# Since we are about to build the model and train it, we will enable the usage of the GPU to make the
-# the process much more quicker.
+"""Since we are about to build the model and train it, we will enable the usage of the GPU to make the
+the process quicker."""
 
 # First we will check if there is any physical GPU detected.
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
-    # Afterwards, attempt to force enable memory growth to prevent so that TensorFlow does not preallocate
+    # Afterwards, attempt to force enable memory growth so that TensorFlow does not preallocate
     # all GPU memory. 
     try:
         for gpu in gpus:
@@ -78,71 +23,87 @@ if gpus:
 else:
     print("No GPUs detected, training will run on CPU.")
 
-# Begin constructing the the training and validations generators using ImageDataGenerator.
-# This is so that the data can be preprocessed and agumentated later on.
+"""Begin constructing the training and validations generators using ImageDataGenerator.
+This is so that the data can be preprocessed and agumentated later on."""
 
 # Define all the paramters that will be used for creating the generators.
-TRAINING_DIR = 'content/cat-breeds-dataset/images'
-IMAGE_SIZE = (200, 200)
+TRAIN_DIR = 'content/catbreedsrefined-7k/train'
+VAL_DIR = 'content/catbreedsrefined-7k/val'
+IMAGE_SIZE = (224, 224)
 BATCH_SIZE = 32
 SEED_SIZE = 42
 
 # Create a data generator for training images.
 train_datagen = ImageDataGenerator(
-  rescale=1./255,
-  validation_split=.2
+    preprocessing_function = preprocess_input,  # Preprocess images to match ResNet50 training format.
+    rotation_range = 10,                        # Small rotations.
+    width_shift_range = 0.05,                   # Horizontal translation.
+    height_shift_range = 0.05,                  # Vertical translation.
+    shear_range= 0.06,                          # Shearing.
+    zoom_range = 0.06,                          # Zoom in/out.
+    horizontal_flip = True,                     # Flip horizontally.
+    brightness_range = [0.8, 1.2],              # Random brightness.
+    fill_mode = 'nearest'                       # Filling missing pixels after transforms.
 )
 
 # Create a data generator for validation images.
 validation_datagen = ImageDataGenerator(
-  rescale=1./255,
-  validation_split=.2
+    preprocessing_function=preprocess_input     # Preprocess images to match ResNet50 training format.
 )
 
 # Load and prepare the training images from the folder.
 train_generator = train_datagen.flow_from_directory(
-  TRAINING_DIR,               # Directory containing image folders
-  target_size=IMAGE_SIZE,     # Resize all images to 100x100
-  subset="training",          # Use the training split
-  batch_size=BATCH_SIZE,      # Number of images per training batch
-  class_mode='sparse',        # Labels are integer class indices
+  TRAIN_DIR,                  # Directory containing image folders.
+  target_size=IMAGE_SIZE,     # Resize all images.
+  batch_size=BATCH_SIZE,      # Number of images per training batch.
+  class_mode='sparse',        # Labels are integer class indices.
   seed=SEED_SIZE,             # Determines the split and shuffle (same for every epoch).
   shuffle=True                # Shuffle training images
 )
 
 # Load and prepare the validation images from the folder.
 validation_generator = validation_datagen.flow_from_directory(
-  TRAINING_DIR,               # Directory containing image folders.
-  target_size=IMAGE_SIZE,     # Resize all images to 100x100.
-  subset="validation",        # Use the validation split.
+  VAL_DIR,                    # Directory containing image folders.
+  target_size=IMAGE_SIZE,     # Resize all images.
   batch_size=BATCH_SIZE,      # Number of images per training batch.
   class_mode='sparse',        # Labels are integer class indices.
   seed=SEED_SIZE,             # Determines the split and shuffle (same for every epoch).
+  shuffle=False               # Does NOT shuffle training images for consistent evaluation/predictions.
 )
 
 # Define all the paramters that will be used for building and training the model.
-EPOCHS_NUM = 20
-CLASSES_NUM = train_generator.num_classes
-LEARNING_RATE = 1e-4
-# Sets mixed_precision to mixed_float16 to improve training speed.
-mixed_precision.set_global_policy('mixed_float16')
+CLASSES_NUM = train_generator.num_classes               # The number of classes retrieved from the generator.
+EPOCHS_NUM = 25                                         # The number of iterations that the model will run.
+LEARNING_RATE = 1e-4                                    # Determines how much the model adjust paramters/weights are updated each step.
+mixed_precision.set_global_policy('mixed_float16')      # Improve training speed.
 
-# Load the ResNet50 base model pretrained on ImageNet.
+# Load the ResNet50 base model.
 base_model = ResNet50(
-    weights='imagenet',
-    include_top=False,
-    input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3)
+    weights='imagenet',                                 # Load the pretrained imagenet weights.
+    include_top=False,                                  # Excludes the final classification layer.
+    input_shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3)       # Tells the model the shape and size of the input.
 )
+
+# Freezes all layers.
+for layer in base_model.layers:
+    layer.trainable = False
 
 # Define the model input.
 inputs = layers.Input(shape=(IMAGE_SIZE[0], IMAGE_SIZE[1], 3))
 x = base_model(inputs, training=True)
-# Add global average pooling and dropout for regularization.
-x = layers.GlobalAveragePooling2D()(x)
-x = layers.Dropout(0.1)(x)
+
+x = layers.GlobalAveragePooling2D()(x)  # Convert feature maps to a single vector per feature (reduces spatial dimensions).
+x = layers.BatchNormalization()(x)      # Normalize activations to improve training stability and speed.
+x = layers.Dropout(0.3)(x)              # Randomly drop 30% of units to reduce overfitting.
 
 # Add the final classification layer with softmax activation.
-outputs = layers.Dense(CLASSES_NUM, activation='softmax')(x)
+# This converts the raw output scores into probabilities that sum to 1.
+# The class with the highest probability is chosen as the model's prediction.
+outputs = layers.Dense(
+    CLASSES_NUM,
+    activation='softmax',
+    kernel_regularizer=regularizers.l2(1e-4)
+    )(x)
 
 # Construct the final model using the input and output layers.
 model = models.Model(inputs, outputs)
@@ -163,4 +124,4 @@ history_fine = model.fit(
 )
 
 # Save the trained model for evaulation or further training.
-model.save(f"model_learning_rate_{LEARNING_RATE}")
+model.save(f"model_v1.1.0.0")
